628f8136d88602810250eb6ac13555fdd28d95fb
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-09-14 05:58:20 -0400

    New interface file added

---
+++ src/iprocessmarketdata.hpp
@@ -0,0 +1,13 @@
+#ifndef _IPROCESSMARKETDATA_HPP_
+#define _IPROCESSMARKETDATA_HPP_
+
+struct IProcessMarketData
+{
+   virtual void Compress( MarketData md, bool eof ) = 0;
+   virtual void Save( const std::string & filename ) = 0;
+   virtual void Load( const std::string & filename ) = 0;
+   virtual void SaveTable( const std::string & filename ) = 0;
+   virtual void LoadTable( const std::string & filename ) = 0;   
+};
+
+#endif // _IPROCESSMARKETDATA_HPP_
