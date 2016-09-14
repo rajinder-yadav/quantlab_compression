42f47d14194dd495432b1e38b82c60f2b5bbd1c4
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-09-14 04:33:01 -0400

    New file added to control tick field bit sizes

---
+++ src/fieldsizes.hpp
@@ -0,0 +1,13 @@
+#ifndef _field_sizes_hpp_
+#define _field_sizes_hpp_
+
+const size_ft buffer_size    = 32; // bits
+const size_ft ticker_size    = 14; // bits
+const size_ft exchange_size  = 1;  // char(8bits)
+const size_ft side_size      = 1;  // char(8bits)
+const size_ft condition_size = 3;  // bits
+const size_ft time_size      = 37; // bits
+const size_ft price_size     = 7;  // char(8bits)
+const size_ft shared_size    = 14; // bits
+
+#endif // _field_sizes_hpp_
