332b24a4817d481a035afb1d00bf2f9b69136182
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-09-14 08:08:40 -0400

    Corrected field side to be 2bits

--- src/fieldsizes.hpp
+++ src/fieldsizes.hpp
@@ -15,7 +15,7 @@
 const size_ft BUFFER_CAPACITY = 32; // bits
 const size_ft TICKER_SIZE     = 14; // bits
 const size_ft EXCHANGE_SIZE   = 1;  // char(8bits)
-const size_ft SIDE_SIZE       = 3;  // bits
+const size_ft SIDE_SIZE       = 2;  // bits
 const size_ft CONDITION_SIZE  = 1;  // char(8bits)
 const size_ft TIME_SIZE       = 37; // bits
 const size_ft PRICE_SIZE      = 8;  // char(8bits)
