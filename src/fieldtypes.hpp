37203a02336d7fe4590384bbc1ca7cc67181f5e8
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-09-14 04:48:52 -0400

    New file that defined BAT field types

---
+++ src/fieldtypes.hpp
@@ -0,0 +1,14 @@
+#ifndef _field_types_hpp_
+#define _field_types_hpp_
+
+typedef uint16_t  ticker_ft;
+typedef char      exchange_ft;
+typedef uint8_t   side_ft;
+typedef char      condition_ft;
+typedef uint64_t  time_ft;
+typedef uint32_t  price_ft;
+typedef uint32_t  share_ft;
+typedef uint32_t  size_ft;
+typedef uint32_t  buffer_ft;
+
+#endif // _field_types_hpp_
